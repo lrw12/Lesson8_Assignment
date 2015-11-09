d92b70987202a21f39309c357532cacbb59cf86f
lindseywg <lrw12@my.fsu.edu>
2015-11-09 17:45:11 +0000

    Lesson 8 Assignment first commit

---
+++ Lesson8_Assignment/script.js
@@ -0,0 +1,21 @@
+/**
+* Created with LIS2360_Lesson8_jQuery.
+* User: lindseywg
+* Date: 2015-11-09
+* Time: 03:00 AM
+* To change this template use Tools | Templates.
+*/
+
+
+      //Project Name:  Recipe Display Application
+      //Author: Lindsey Wilson-Griffith
+      //Date: November 13, 2015   
+
+      //Filename: scrip.js
+   
+function display(event) {
+  $(event.currentTarget).next().fadeIn("slow"); 
+  $("h3").click.(display);
+}//end of display() 
+
+  
