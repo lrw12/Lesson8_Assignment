b7757f707cdd8f9c57e4321d7e070c609e258ebe
lindseywg <lrw12@my.fsu.edu>
2015-11-12 21:11:31 +0000

    first commit

--- Lesson8_Assignment/script.js
+++ Lesson8_Assignment/script.js
@@ -15,8 +15,28 @@
    
 function display(event) {
   $(event.currentTarget).next().fadeIn("slow"); 
-  $("h3").click.(display);
 }//end of display() 
 
-  //
-  //
+/*
+$("h3").click(display);
+*/
+
+function display2(event) {
+  $(event.currentTarget).next().animate( { height: 'toggle'}, "slow");
+}
+
+/*
+$("h3").click(display2);
+*/
+
+$("h3").hover(function(){
+  $(this).css("background-color", "yellow");
+  }, function(){
+  $(this).css("background-color", "pink");
+});
+
+$("h3").hover(display2);
+
+
+
+ 
